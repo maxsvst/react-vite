9a066c7de4964cf77cca2656729b8e4215ab278b
maxsvst <maksim.feklin@simbirsoft.com>
2025-03-03 13:10:05 +0300

    Верстка корневого экрана и настройка роутинга

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+
+import { WelcomeScreen } from "./pages/welcome-screen/WelcomeScreen";
+
+import styles from "./App.module.scss";
+import { Header } from "./components/header/Header";
+
 function App() {
-  return <></>;
+  return (
+    <div className={styles.appContainer}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Header />}>
+            <Route index element={<WelcomeScreen />} />
+            <Route
+              path="/info"
+              element={
+                <>
+                  <div>INFO</div>
+                </>
+              }
+            />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
